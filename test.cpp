47244d4624d699e69684aa78bf72498df4c6c8e1
hcz <h.cz@qq.com>
2015-11-01 16:13:42 +0800

    add util.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "util.hpp"
+
 #include <sstream>
 #include <iostream>
 
--- util.hpp
+++ util.hpp
@@ -7,54 +7,16 @@
 
 namespace util {
 
-std::string cCode(bool &value) {
-    return value ? "true" : "false";
-}
+std::string cCode(bool &value);
+std::string cCode(int64_t &value);
+std::string cCode(double &value);
+std::string cCode(std::string &value);
+std::string cCode(uintptr_t &value);
 
-std::string cCode(int64_t &value) {
-    return std::to_string(value);
-}
-
-std::string cCode(double &value) {
-    // return std::to_string(value);
-    return "*(double *) (uint64_t []) {"
-        + std::to_string(*(uint64_t *) &value) + "}";
-}
-
-std::string cCode(std::string &value) {
-    std::string result;
-
-    result += "(const char []) {";
-    for (char i: value) {
-        result += std::to_string((int) i) + ",";
-    }
-    result += "}";
-
-    return result;
-}
-
-std::string cCode(uintptr_t &value) {
-    return std::to_string(value);
-}
-
-std::string cType(std::string &&name, bool &value) {
-    return "bool " + name;
-}
-
-std::string cType(std::string &&name, int64_t &value) {
-    return "int64_t " + name;
-}
-
-std::string cType(std::string &&name, double &value) {
-    return "double " + name;
-}
-
-std::string cType(std::string &&name, std::string &value) {
-    return "const char " + name + "[]";
-}
-
-std::string cType(std::string &&name, uintptr_t &value) {
-    return "uintptr_t " + name;
-}
+std::string cType(std::string &&name, bool &value);
+std::string cType(std::string &&name, int64_t &value);
+std::string cType(std::string &&name, double &value);
+std::string cType(std::string &&name, std::string &value);
+std::string cType(std::string &&name, uintptr_t &value);
 
 }
