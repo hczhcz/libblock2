1149a25a59c670207ca60f7b11fb843f3876bd29
hcz <h.cz@qq.com>
2015-11-22 00:20:38 +0800

    split lib_std.cpp

--- lib_io.cpp
+++ lib_io.cpp
@@ -6,42 +6,6 @@ namespace lib {
 
 using namespace builder;
 
-class Then: public BlockBuiltin {
-protected:
-    virtual void inSpecialArg(
-        Instance &caller, Instance &,
-        size_t, std::unique_ptr<Node> &arg,
-        Output &output,
-        std::function<std::string ()> &&
-    ) {
-        arg->buildProc(caller, output);
-    }
-
-    virtual void buildContent(Instance &, Output &) {
-        // nothing
-    }
-
-public:
-    Then():
-        BlockBuiltin {
-            {BlockOption::allow_proc},
-            {}
-        } {}
-};
-Builtin __then__ {"__then__", new Then {}};
-
-Builtin __set__ {"__set__",
-    new BlockBuiltinFmt {
-        {BlockOption::allow_proc},
-        {out("dest"), in("src")},
-        {},
-        {
-            {"dest", "src"}
-        },
-        "$dest = $src;"
-    }
-};
-
 Builtin print {"print",
     new BlockBuiltinFmt {
         {BlockOption::allow_proc},
