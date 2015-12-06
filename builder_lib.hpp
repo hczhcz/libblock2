7f69923a8dcdfa172c611354d10482cd8e58c2ef
hcz <h.cz@qq.com>
2015-12-06 23:19:48 +0800

    add libFunc builders

--- builder_lib.hpp
+++ builder_lib.hpp
@@ -14,7 +14,67 @@ std::function<Block *()> libFuncT() {
     };
 }
 
-inline std::function<Block *()> libFunc2R(std::string &&str) {
+inline std::function<Block *()> libFuncB2R(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_out},
+            {in("a")},
+            {typeBool("a"), typeBool("result")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncB2A(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {out("a")},
+            {typeBool("input"), typeBool("a")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncB3R(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_out},
+            {in("a"), in("b")},
+            {typeBool("a"), typeBool("b"), typeBool("result")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncB3A(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {out("a"), in("b")},
+            {typeBool("input"), typeBool("a"), typeBool("b")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncB3B(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {in("a"), out("b")},
+            {typeBool("input"), typeBool("a"), typeBool("b")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncI2R(std::string &&str) {
     return [str = std::move(str)]() mutable {
         return new BlockBuiltinFmt {
             {BlockOption::allow_out},
@@ -26,7 +86,7 @@ inline std::function<Block *()> libFunc2R(std::string &&str) {
     };
 }
 
-inline std::function<Block *()> libFunc2A(std::string &&str) {
+inline std::function<Block *()> libFuncI2A(std::string &&str) {
     return [str = std::move(str)]() mutable {
         return new BlockBuiltinFmt {
             {BlockOption::allow_in},
@@ -38,7 +98,7 @@ inline std::function<Block *()> libFunc2A(std::string &&str) {
     };
 }
 
-inline std::function<Block *()> libFunc3R(std::string &&str) {
+inline std::function<Block *()> libFuncI3R(std::string &&str) {
     return [str = std::move(str)]() mutable {
         return new BlockBuiltinFmt {
             {BlockOption::allow_out},
@@ -50,7 +110,7 @@ inline std::function<Block *()> libFunc3R(std::string &&str) {
     };
 }
 
-inline std::function<Block *()> libFunc3A(std::string &&str) {
+inline std::function<Block *()> libFuncI3A(std::string &&str) {
     return [str = std::move(str)]() mutable {
         return new BlockBuiltinFmt {
             {BlockOption::allow_in},
@@ -62,12 +122,72 @@ inline std::function<Block *()> libFunc3A(std::string &&str) {
     };
 }
 
-inline std::function<Block *()> libFunc3B(std::string &&str) {
+inline std::function<Block *()> libFuncI3B(std::string &&str) {
     return [str = std::move(str)]() mutable {
         return new BlockBuiltinFmt {
             {BlockOption::allow_in},
             {in("a"), out("b")},
             {typeInt("input"), typeInt("a"), typeInt("b")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncR2R(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_out},
+            {in("a")},
+            {typeReal("a"), typeReal("result")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncR2A(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {out("a")},
+            {typeReal("input"), typeReal("a")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncR3R(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_out},
+            {in("a"), in("b")},
+            {typeReal("a"), typeReal("b"), typeReal("result")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncR3A(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {out("a"), in("b")},
+            {typeReal("input"), typeReal("a"), typeReal("b")},
+            {},
+            std::move(str)
+        };
+    };
+}
+
+inline std::function<Block *()> libFuncR3B(std::string &&str) {
+    return [str = std::move(str)]() mutable {
+        return new BlockBuiltinFmt {
+            {BlockOption::allow_in},
+            {in("a"), out("b")},
+            {typeReal("input"), typeReal("a"), typeReal("b")},
             {},
             std::move(str)
         };
