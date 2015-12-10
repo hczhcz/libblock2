4938b4d15bd7727d422e608a238b08caf96ed2df
hcz <h.cz@qq.com>
2015-12-10 13:06:42 +0800

    add gc_util.hpp

--- include.hpp
+++ include.hpp
@@ -8,17 +8,11 @@
 
     #include <functional>
     #include <string>
-    #include <list>
-    #include <vector>
     #include <set>
-    #include <map>
     #include <fstream>
     #include <iostream>
 
-    #include <gc/gc.h>
-    #include <gc/gc_cpp.h>
-    #include <gc/gc_allocator.h>
-
+    #include "./lib/gc_util.hpp"
     #include "./lib/fork_try.hpp"
     #include "./lib/myparser/myparser.hpp"
 
@@ -26,17 +20,3 @@
         #include "lbstd.h"
     }
 #endif
-
-// TODO
-namespace std {
-    template <class T>
-    using gc_list = list<T, gc_allocator<T>>;
-    template <class T>
-    using gc_vector = vector<T, gc_allocator<T>>;
-    template <class Key, class T>
-    using gc_map = map<
-        Key, T,
-        less<Key>,
-        gc_allocator<std::pair<const Key, T>>
-    >;
-}
---
+++ lib/gc_util.hpp
@@ -0,0 +1,51 @@
+#include <list>
+#include <vector>
+#include <map>
+
+#include <gc/gc.h>
+#include <gc/gc_cpp.h>
+#include <gc/gc_allocator.h>
+
+namespace std {
+
+template <class T>
+using gc_list = list<T, gc_allocator<T>>;
+template <class T>
+using gc_vector = vector<T, gc_allocator<T>>;
+template <class Key, class T>
+using gc_map = map<
+    Key, T,
+    less<Key>,
+    gc_allocator<std::pair<const Key, T>>
+>;
+
+template <class T>
+class gc_function;
+
+template <class R, class... Args>
+class gc_function<R (Args...)>: public function<R (Args...)> {
+public:
+    template <class Func>
+    gc_function(Func _func):
+        function<R (Args...)> {
+            std::allocator_arg,
+            gc_allocator<char> {},
+            std::forward<Func>(_func)
+        } {}
+
+    gc_function(const gc_function &_obj):
+        function<R (Args...)> {
+            std::allocator_arg,
+            gc_allocator<char> {},
+            static_cast<const function<R (Args...)> &>(_obj)
+        } {}
+
+    gc_function(gc_function &&_obj):
+        function<R (Args...)> {
+            std::allocator_arg,
+            gc_allocator<char> {},
+            static_cast<function<R (Args...)> &&>(_obj)
+        } {}
+};
+
+}
