09ddf586975ea7def3c1e2c0c48d5ba0147158b0
hcz <h.cz@qq.com>
2015-12-12 16:09:54 +0800

    modify

--- lbexec.h
+++ lbexec.h
@@ -3,10 +3,8 @@
 #include "lbstd.h"
 
 #define LB_CALL() \
-    self->func(self, callee, inner, tmp);
+    return self->func(self, callee, inner, tmp);
 
 #define LB_ENTER(func) LB_FUNC(func) {
 #define LB_EXIT() LB_CALL() }
-
-#define LB_RETURN() LB_CALL() return;
 #define LB_YIELD(func) LB_CALL() } LB_FUNC(func) {
